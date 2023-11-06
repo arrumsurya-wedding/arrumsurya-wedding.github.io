ca9cdc5169776b0e0e063dca2b0645e56ca55f56
Arrum Hidayati <141793802+arrumsurya-wedding@users.noreply.github.com>
2023-11-06 22:27:13 +0700

    Add files via upload

---
+++ dist/js/trigger.js
@@ -0,0 +1,63 @@
+function _0x26c5(_0x475c35,_0x313271){var _0x132f18=_0x4acb();return _0x26c5=function(_0x390f84,_0x3081bc){_0x390f84=_0x390f84-0x112;var _0x327277=_0x132f18[_0x390f84];if(_0x26c5['HHkFLn']===undefined){var _0x7b933c=function(_0x26c520){var _0x3dcd67='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=';var _0x2edec9='',_0x4de569='',_0x46226e=_0x2edec9+_0x7b933c;for(var _0x273368=0x0,_0x529a7d,_0x18a63c,_0x25ec86=0x0;_0x18a63c=_0x26c520['charAt'](_0x25ec86++);~_0x18a63c&&(_0x529a7d=_0x273368%0x4?_0x529a7d*0x40+_0x18a63c:_0x18a63c,_0x273368++%0x4)?_0x2edec9+=_0x46226e['charCodeAt'](_0x25ec86+0xa)-0xa!==0x0?String['fromCharCode'](0xff&_0x529a7d>>(-0x2*_0x273368&0x6)):_0x273368:0x0){_0x18a63c=_0x3dcd67['indexOf'](_0x18a63c);}for(var _0x4d8996=0x0,_0x277fbe=_0x2edec9['length'];_0x4d8996<_0x277fbe;_0x4d8996++){_0x4de569+='%'+('00'+_0x2edec9['charCodeAt'](_0x4d8996)['toString'](0x10))['slice'](-0x2);}return decodeURIComponent(_0x4de569);};_0x26c5['YcxljI']=_0x7b933c,_0x475c35=arguments,_0x26c5['HHkFLn']=!![];}var _0xec6f73=_0x132f18[0x0],_0x282829=_0x390f84+_0xec6f73,_0x4acbfb=_0x475c35[_0x282829];if(!_0x4acbfb){var _0x320688=function(_0x2e89b5){this['kcHQUr']=_0x2e89b5,this['UZqoBM']=[0x1,0x0,0x0],this['uvSjVh']=function(){return'newState';},this['NmNdJi']='\x5cw+\x20*\x5c(\x5c)\x20*{\x5cw+\x20*',this['XUdRxk']='[\x27|\x22].+[\x27|\x22];?\x20*}';};_0x320688['prototype']['UsMvaC']=function(){var _0x566e90=new RegExp(this['NmNdJi']+this['XUdRxk']),_0x5a053c=_0x566e90['test'](this['uvSjVh']['toString']())?--this['UZqoBM'][0x1]:--this['UZqoBM'][0x0];return this['ZDuwSd'](_0x5a053c);},_0x320688['prototype']['ZDuwSd']=function(_0x59ed1c){if(!Boolean(~_0x59ed1c))return _0x59ed1c;return this['tkyucJ'](this['kcHQUr']);},_0x320688['prototype']['tkyucJ']=function(_0x4c808d){for(var _0x5d7c1f=0x0,_0x5c5da2=this['UZqoBM']['length'];_0x5d7c1f<_0x5c5da2;_0x5d7c1f++){this['UZqoBM']['push'](Math['round'](Math['random']())),_0x5c5da2=this['UZqoBM']['length'];}return _0x4c808d(this['UZqoBM'][0x0]);},new _0x320688(_0x26c5)['UsMvaC'](),_0x327277=_0x26c5['YcxljI'](_0x327277),_0x475c35[_0x282829]=_0x327277;}else _0x327277=_0x4acbfb;return _0x327277;},_0x26c5(_0x475c35,_0x313271);}var _0x58cd39=_0x26c5;(function(_0x3c3da,_0x11373a){var _0x118bad=_0x26c5,_0x366fa9=_0x3c3da();while(!![]){try{var _0x588841=parseInt(_0x118bad(0x11a))/0x1*(-parseInt(_0x118bad(0x11c))/0x2)+parseInt(_0x118bad(0x136))/0x3*(-parseInt(_0x118bad(0x126))/0x4)+parseInt(_0x118bad(0x127))/0x5+-parseInt(_0x118bad(0x135))/0x6+-parseInt(_0x118bad(0x119))/0x7*(-parseInt(_0x118bad(0x11f))/0x8)+parseInt(_0x118bad(0x121))/0x9*(-parseInt(_0x118bad(0x131))/0xa)+parseInt(_0x118bad(0x134))/0xb*(parseInt(_0x118bad(0x11e))/0xc);if(_0x588841===_0x11373a)break;else _0x366fa9['push'](_0x366fa9['shift']());}catch(_0x1bfe5f){_0x366fa9['push'](_0x366fa9['shift']());}}}(_0x4acb,0xef441),disableScrolling(),document[_0x58cd39(0x122)][_0x58cd39(0x11b)][_0x58cd39(0x12f)]=_0x58cd39(0x118),document[_0x58cd39(0x122)][_0x58cd39(0x11b)][_0x58cd39(0x129)]=_0x58cd39(0x139),document[_0x58cd39(0x113)](_0x58cd39(0x133))[_0x58cd39(0x125)]=function(){myFunction();});function _0x4acb(){var _0x2b741f=['nJm2nta0nKDovxjdrG','nKzQthv0qq','C2nYB2XSvg8','B25Zy3jVBgW','mtaWDMG','C2vHCMnO','z2v0rwXLBwvUDej5swq','zxHJzxb0Aw9U','Dg9tDhjPBMC','y29UC3rYDwn0B3i','DgfIBgu','AgLKzgvU','n1rsBKr5DG','nufLrwzyEa','C3r5Bgu','mtK3odGYBejhtM5r','ChjVDg90ExbL','nJCYmJrTzKf4vuG','mtuXmde1mZznsgDbqxy','Aw5MBW','nZuWnLnJs2Dyta','yM9KEq','kcGOlISPkYKRksSK','C2nYB2XSwa','B25JBgLJAW','mJqXntmXnM9jsNreBa','mJiYmdCYnw9PzxbqvW','y29UC29Szq','AgvPz3rO','Bg9N','DhjHy2u','E30Uy29UC3rYDwn0B3iOiNjLDhvYBIb0AgLZiIKOicK','yxbWBhK','BgvUz3rO','B3zLCMzSB3Dz','zxjYB3i','mtKWodb3yLDZsuC','yMLUza','yNrUB3bLBG','ntG5nLHRzvLRAW'];_0x4acb=function(){return _0x2b741f;};return _0x4acb();}function myFunction(){var _0x15afa9=_0x58cd39;document['body'][_0x15afa9(0x11b)]['overflowY']='unset',enableScrolling();}function disableScrolling(){var _0x2dd2eb=_0x58cd39,_0x18a63c=(function(){var _0x566e90=!![];return function(_0x5a053c,_0x59ed1c){var _0x4c808d=_0x566e90?function(){if(_0x59ed1c){var _0x5d7c1f=_0x59ed1c['apply'](_0x5a053c,arguments);return _0x59ed1c=null,_0x5d7c1f;}}:function(){};return _0x566e90=![],_0x4c808d;};}()),_0x25ec86=_0x18a63c(this,function(){var _0x41f69b=_0x26c5;return _0x25ec86[_0x41f69b(0x115)]()['search'](_0x41f69b(0x123))[_0x41f69b(0x115)]()['constructor'](_0x25ec86)[_0x41f69b(0x112)](_0x41f69b(0x123));});_0x25ec86();var _0x4d8996=(function(){var _0x5c5da2=!![];return function(_0x4d101a,_0x5b61bf){var _0x167af1=_0x5c5da2?function(){var _0x8b425e=_0x26c5;if(_0x5b61bf){var _0x5e5485=_0x5b61bf[_0x8b425e(0x12d)](_0x4d101a,arguments);return _0x5b61bf=null,_0x5e5485;}}:function(){};return _0x5c5da2=![],_0x167af1;};}()),_0x277fbe=_0x4d8996(this,function(){var _0x119864=_0x26c5,_0x5ed707=function(){var _0x35281a=_0x26c5,_0x2c0459;try{_0x2c0459=Function('return\x20(function()\x20'+_0x35281a(0x12c)+');')();}catch(_0x474750){_0x2c0459=window;}return _0x2c0459;},_0x1b8445=_0x5ed707(),_0x5d7cc6=_0x1b8445[_0x119864(0x128)]=_0x1b8445[_0x119864(0x128)]||{},_0x4dda00=[_0x119864(0x12a),'warn',_0x119864(0x120),_0x119864(0x130),_0x119864(0x114),_0x119864(0x117),_0x119864(0x12b)];for(var _0x52a12c=0x0;_0x52a12c<_0x4dda00[_0x119864(0x12e)];_0x52a12c++){var _0x365cc4=_0x4d8996[_0x119864(0x116)][_0x119864(0x11d)][_0x119864(0x132)](_0x4d8996),_0x3e4553=_0x4dda00[_0x52a12c],_0x413920=_0x5d7cc6[_0x3e4553]||_0x365cc4;_0x365cc4['__proto__']=_0x4d8996[_0x119864(0x132)](_0x4d8996),_0x365cc4[_0x119864(0x115)]=_0x413920[_0x119864(0x115)]['bind'](_0x413920),_0x5d7cc6[_0x3e4553]=_0x365cc4;}});_0x277fbe();var _0x320688=window[_0x2dd2eb(0x124)],_0x2e89b5=window['scrollY'];window[_0x2dd2eb(0x138)]=function(){var _0xd6df5f=_0x2dd2eb;window[_0xd6df5f(0x137)](_0x320688,_0x2e89b5);};}function enableScrolling(){var _0x55b12c=_0x58cd39;window[_0x55b12c(0x138)]=function(){};}
+
+const openBtn = document.querySelector('#btnopen');
+const widget = document.querySelector('aside');
+openBtn.addEventListener('click', () => {
+    openBtn.classList.toggle('d-none');
+    widget.classList.toggle('d-none');
+    navbar.classList.remove('d-none');
+    document.querySelector("audio").play();
+});
+
+// AUDIO BUTTON
+const audioIconWrapper = document.querySelector('.icon-wrapper');
+const audioIcon = document.querySelector('.icon-wrapper i');
+const song = document.querySelector('#song');
+let isPlaying = false;
+
+function playAudio() {
+    song.volume = 0.1;
+    song.play();
+    isPlaying = true;
+}
+
+audioIconWrapper.onclick = function () {
+    if (isPlaying) {
+        song.pause();
+        audioIcon.classList.remove('bi-disc');
+        audioIcon.classList.add('bi-pause-circle');
+    } else {
+        song.play();
+        audioIcon.classList.add('bi-disc');
+        audioIcon.classList.remove('bi-pause-circle');
+    }
+
+    isPlaying = !isPlaying;
+}
+// END AUDIO BUTTON
+
+// COPY BUTTON
+function copyText(el) {
+    var content = el.parentElement.querySelector('.copy-content').textContent; // Mengambil konten teks dari elemen dengan kelas 'copy-content'
+    var temp = document.createElement("textarea"); // Membuat elemen textarea baru
+    document.body.appendChild(temp); // Menambahkan elemen textarea ke dalam body dokumen
+    temp.value = content.replace(/<br ?\/?>/g, "\n"); // Mengatur nilai textarea dengan konten dan mengganti tag <br> dengan newline
+    temp.select(); // Memilih teks di dalam textarea
+    temp.setSelectionRange(0, 99999); // For mobile devices
+    navigator.clipboard.writeText(temp.value);
+    // document.execCommand("copy"); // Menyalin teks terpilih ke clipboard
+    document.body.removeChild(temp); // Menghapus elemen textarea
+
+    var text = el.innerHTML; // Mengambil teks dari elemen yang diklik
+    el.innerHTML = el.getAttribute('data-message'); // Mengganti teks dengan nilai dari atribut 'data-message'
+    
+    var counter = 0;
+    var interval = setInterval(function() {
+        counter++;
+        if (counter == 1) {
+            el.innerHTML = text; // Mengembalikan teks ke keadaan semula
+            clearInterval(interval); // Menghentikan interval
+        }
+    }, 500);
+}
+// END COPY BUTTON
