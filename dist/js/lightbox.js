449f3f9121eea4bc7cfec7988532e378383f5ed1
Arrum Hidayati <141793802+arrumsurya-wedding@users.noreply.github.com>
2023-12-23 14:02:22 +0700

    Update lightbox.js

--- dist/js/lightbox.js
+++ dist/js/lightbox.js
@@ -3,8 +3,8 @@ import lgZoom from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/zoom";
 import lgThumbnail from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/thumbnail";
 import lgShare from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/share";
 import lgRotate from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/rotate";
-// import lgAutoplay from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/autoplay";
 import lgFullscreen from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/fullscreen";
+// import lgAutoplay from "https://cdn.skypack.dev/lightgallery@2.1.2/plugins/autoplay";
 
 const selectedGallery = "[data-lightbox-thumbnail]";
 const carousels = document.querySelectorAll("[data-lightbox-gallery]");
@@ -12,6 +12,8 @@ const carousels = document.querySelectorAll("[data-lightbox-gallery]");
 const lightboxConfig = {
 	autoplayFirstVideo: true,
 	galleryId: "nature",
+	showZoomInOutIcons: true,
+	actualSize: false,
 	flipHorizontal: false,
 	flipVertical: false,
 	rotateLeft: false,
